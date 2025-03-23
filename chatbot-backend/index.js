ab8f0242cb6d1032fc5cc03b2c95779d810d3e7d
Abhishek Kumar <abhisheklenovo2001@gmail.com>
2025-03-23 12:01:25 +0530

    vercel for backend updated

--- chatbot-backend/index.js
+++ chatbot-backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const serverless = require("serverless-http");
 
 dotenv.config();
 connectDB();
@@ -13,7 +14,12 @@ app.use(cors());
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/logs", require("./routes/logRoutes"));
 
-app.get("/", (req, res) => res.send("API Running"));
+app.get("/", (req, res) => res.send("Backend API Running"));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(5000, () =>
+    console.log(`Server running on port 5000`)
+);
+
+// Export the app for Vercel (Serverless function)
+module.exports = app;
+module.exports.handler = serverless(app);
